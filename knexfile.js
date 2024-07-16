ee9ea8ace8897421cfe700253ea9fe72585e174d
DESKTOP-51GC2CI\Danny <danny>
2024-07-16 08:39:02 -0500

    v4

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,8 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: "127.0.0.1",
-      user: "root",
-      password: "",
+      user: "api_user2@localhost",
+      password: "123456",
       database: "cloud2"
     },
     migrations: {
