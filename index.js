d5e6b33bd4eb2a7464e0c624b260ea7eade36fbb
DESKTOP-51GC2CI\Danny <dannysimba2602@gmail.com>
2024-07-23 08:13:56 -0500

    v3

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express =require('express');
 const knex=require('./db')//llama la conf de db
 const routes=require('./routes')//llamada a routes
+const cors = require('cors'); // Importa el paquete cors
 
-const app=express();//creamos nueva instancia 
-const port=3001;//puerto de salida
+const app = express();//creamos nueva instancia 
+const port = 3001;//puerto de salida
 
+app.use(cors()); // Usa el middleware cors
 app.use(express.json());//configura tipo json
 
 app.use('/api',routes);//configura la url base y rutas
